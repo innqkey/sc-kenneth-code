1bcdd01d8ebf6f67aaf5c5e9b9cc6b634b8cd94b
qinkai <842170861@qq.com>
2019-02-19 15:41:59 +0800

    init

---
+++ sc-eureka-server/src/main/java/com/kenneth/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.kenneth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author qinkai
+ * @date 2019年1月25日
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
