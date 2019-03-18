d627dc1e99dc3d2c2beac227eff026d5dcca6604
qinkai <15869129921@139.com>
2019-03-18 15:05:57 +0800

    add config server and client

---
+++ sc-config-client/src/main/java/com/kenneth/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.kenneth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+ * @author: qin kai
+ * @Date: 2019年3月18日 上午11:11:53
+ */
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class,args);
+	}
+}
---
+++ sc-config-client/src/main/java/com/kenneth/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.kenneth.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author qinkai
+ * @date 2019年2月20日
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+	
+	@Value("${name}")
+	private String foo;
+	
+	@GetMapping(value = "/hi")
+	public String hi() {
+		return foo;
+	}
+}
