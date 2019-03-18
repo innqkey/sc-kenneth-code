f6aa44eea6c3ae02b7b3aac6462fea57a5802259
qinkai <15869129921@139.com>
2019-03-18 22:16:58 +0800

    config -- server -- cluster

--- sc-config-server/src/main/java/com/kenneth/ConfigServerApplication.java
+++ sc-config-server/src/main/java/com/kenneth/ConfigServerApplication.java
@@ -2,18 +2,18 @@ package com.kenneth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/** 
- * @author: qin kai
- * @Date: 2019年3月18日 上午11:11:53
- */
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class,args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
