4b743e3cf4a710e013274194d8994cca6679cb19
qinkai <15869129921@139.com>
2019-03-29 18:53:57 +0800

    hystrix 高可用

--- sc-hystrix-turbine/src/main/java/com/kenneth/ServiceHystrixTurbine.java
+++ sc-hystrix-turbine/src/main/java/com/kenneth/ServiceHystrixTurbine.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,15 +22,11 @@ import org.springframework.web.client.RestTemplate;
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
-public class EurekaServiceHystrix {
+@EnableTurbine
+public class ServiceHystrixTurbine {
     
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceHystrix.class, args);
+        SpringApplication.run(ServiceHystrixTurbine.class, args);
     }
     
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
 }
